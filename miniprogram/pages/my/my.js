00d0bfacd237c57fca6d769c54154e7e145a4833
风的颜色 <35207265+husanfeng@users.noreply.github.com>
2019-07-14 00:18:35 +0800

    部署第一个云函数：登录函数，并调用

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -89,5 +89,25 @@ Page({
         wx.hideLoading()
       }
     })
+  },
+  getUserOpenId(){
+    wx.showLoading({
+      title: '正在加载...',
+    })
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      },
+      complete: res => {
+        wx.hideLoading()
+      }
+    })
   }
 })
