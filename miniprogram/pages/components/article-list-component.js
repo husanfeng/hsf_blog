e95d1e58a4adc99f4bd69a9a49c358dbdf2c6dc2
风的颜色 <35207265+husanfeng@users.noreply.github.com>
2019-07-21 20:34:53 +0800

    文章列表组件更新

--- miniprogram/pages/components/article-list-component.js
+++ miniprogram/pages/components/article-list-component.js
@@ -6,16 +6,15 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    articleList:{
-      type:Object,
-      value:[]
+    articleList: {
+      type: Object,
+      value: []
     },
   },
   /**
    * 组件的初始数据
    */
-  data: {
-  },
+  data: {},
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
     attached: function() {},
@@ -25,7 +24,7 @@ Component({
   // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
   attached: function() {}, // 此处attached的声明会被lifetimes字段中的声明覆盖
   ready: function() {
-  
+
   },
   pageLifetimes: {
     // 组件所在页面的生命周期函数
@@ -37,6 +36,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    click(e) {
+      var id = e.currentTarget.dataset.id; // 文章的唯一标识
+      var className = e.currentTarget.dataset.className;  // 文章的分类名称
+      var readCount = e.currentTarget.dataset.readCount
+      wx.navigateTo({
+        url: '../articleDetail/articleDetail?id=' + id + '&className=' + className + '&readCount=' + readCount,
+      })
+    },
   },
- 
+
 })
