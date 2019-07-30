70a4cde16a47796803a7fbcf2ed1076bdf0152d8
husanfeng <18755106041@163.com>
2019-07-30 17:48:00 +0800

    页面滑动底部实现加载更多功能

--- miniprogram/pages/articleList/articleList.js
+++ miniprogram/pages/articleList/articleList.js
@@ -11,7 +11,15 @@ Page({
     class_id: '',
     name: '',
     filter: {},
-    userInfo:{}
+    userInfo: {},
+    dbName: '',
+
+
+    loading: false, // 正在加载
+    loadingHasData: true, //是否还有有数据
+    size: 10,
+    page: 1,
+    dataList: [],
   },
 
   /**
@@ -23,22 +31,71 @@ Page({
     var type = options.type ? options.type : '' // 分辨参数（我的浏览，我的点赞...）
     var userInfo = wx.getStorageSync("userInfo");
     this.setData({
-      class_id: class_id,
-      userInfo: userInfo
+
     })
     wx.setNavigationBarTitle({
       title: options.name ? options.name : options.type
     })
     if (type == '我的浏览') {
-      this.queryVisit()
-    } else if (type == '我的点赞'){
-      this.queryPollList()
-    } 
-    else {
-      this.initArticleList(class_id)
+      // this.queryVisit()
+      this.setData({
+        dbName: 'browsing_volume',
+        userInfo: userInfo
+      })
+      this.fetchSearchList(true);
+    } else if (type == '我的点赞') {
+      // this.queryPollList()
+      this.setData({
+        dbName: 'poll',
+        userInfo: userInfo
+      })
+      this.fetchSearchList(true);
+    } else {
+      // this.initArticleList(class_id) // 分类页面过来的
+      this.setData({
+        dbName: 'article',
+        userInfo: userInfo,
+        filter: {
+          class_id: parseInt(class_id)
+        }
+      })
+      this.fetchSearchList(true);
     }
   },
-  queryPollList(){
+  /**
+   * 分页函数
+   */
+  fetchSearchList: function(isShowLoading) {
+    if (isShowLoading) {
+      wx.showLoading({
+        title: '正在请求数据...',
+      });
+    }
+    let that = this;
+    this.initArticleList(function(data) {
+      if (isShowLoading) {
+        wx.hideLoading()
+      }
+      //判断是否有数据，有则取数据  
+      if (data.length != 0) {
+        let dataList = [];
+        //如果isFromSearch是true从data中取出数据，否则先从原来的数据继续添加  
+        dataList = that.data.dataList.length <= 0 ? data : that.data.dataList.concat(data)
+        that.setData({
+          dataList: dataList, //获取数据数组  
+          loadingHasData: true,
+          loading: false,
+          page: (that.data.page + 1)
+        });
+      } else {
+        that.setData({
+          loading: false,
+          loadingHasData: false
+        });
+      }
+    })
+  },
+  queryPollList() {
     var _this = this;
     _this.setData({ // 根据我的浏览查询文章
       filter: {
@@ -87,8 +144,8 @@ Page({
       name: 'getArticleListData',
       data: {
         dbName: 'browsing_volume',
-        pageIndex: 1,
-        pageSize: 10,
+        pageIndex: _this.data.page,
+        pageSize: _this.data.size,
         filter: _this.data.filter,
       },
       success: res => {
@@ -106,88 +163,57 @@ Page({
       }
     })
   },
-  initArticleList(id) {
+  initArticleList(callback) {
     var _this = this;
-    if (_this.data.class_id != '') { // 根据分类查询文章
-      this.setData({
-        filter: {
-          class_id: parseInt(_this.data.class_id)
-        }
-      })
-    }
-    wx.showLoading({
-      title: '正在加载...',
-    })
     // 调用云函数
     wx.cloud.callFunction({
       name: 'getArticleListData',
       data: {
-        dbName: 'article',
-        pageIndex: 1,
-        pageSize: 10,
+        dbName: _this.data.dbName,
+        pageIndex: _this.data.page,
+        pageSize: _this.data.size,
         filter: _this.data.filter,
       },
       success: res => {
         // res.data 包含该记录的数据
         console.log(res.result.data)
-        _this.setData({
-          articleList: _this.data.articleList.concat(res.result.data)
-        })
+        callback(res.result.data);
       },
       fail: err => {
         console.error('[云函数]调用失败', err)
       },
       complete: res => {
-        wx.hideLoading()
+
       }
     })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    wx.stopPullDownRefresh() //停止下拉刷新
+    this.setData({
+      loading: false, // 正在加载
+      loadingHasData: true, //是否还有有数据
+      page: 0,
+      dataList: [],
+    });
+    this.fetchSearchList(true);
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
+    if (this.data.loading)
+      return;
+    if (!this.data.loadingHasData)
+      return;
+    this.setData({
+      loading: true,
+      loadingHasData: true,
+    });
+    this.fetchSearchList(false);
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -15,20 +15,20 @@ Page({
     loading: false, // 正在加载
     loadingHasData: true, //是否还有有数据
     size: 10,
-    page: 0,
+    page: 1,
     dataList: [],
   },
-
   onLoad: function() {
     this.getUserOpenId()
     this.initSwiper();
     this.initClassfication();
+    this.fetchSearchList(true);
     var date = new Date('2019-07-27 08:25:40');
     // 有三种方式获取
     var time1 = date.getTime();
     var time2 = date.valueOf();
     var time3 = Date.parse(date);
-    time3 = time3/1000;
+    time3 = time3 / 1000;
     // console.log(time1);//1398250549123
     // console.log(time2);//1398250549123
     // console.log(time3);//1398250549000
@@ -57,32 +57,27 @@ Page({
       }
     })
   },
-  initArticleList() {
+  initArticleList(callback) {
     var _this = this;
-    wx.showLoading({
-      title: '正在加载...',
-    })
     // 调用云函数
     wx.cloud.callFunction({
       name: 'getArticleListData',
       data: {
         dbName: 'article',
-        pageIndex: 1,
-        pageSize: 10,
+        pageIndex: _this.data.page,
+        pageSize: _this.data.size,
         // filter: {},
       },
       success: res => {
         // res.data 包含该记录的数据
-        console.log(res.result.data)
-        _this.setData({
-          articleList: res.result.data
-        })
+        console.log(res.result)
+        callback(res.result.data);
       },
       fail: err => {
         console.error('[云函数]调用失败', err)
       },
       complete: res => {
-        wx.hideLoading()
+
       }
     })
   },
@@ -136,23 +131,57 @@ Page({
     })
   },
   onShow() {
-    this.setData({
-      articleList: []
-    })
-    this.initArticleList();
+    // this.setData({
+    //   articleList: []
+    // })
+
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
+    wx.stopPullDownRefresh() //停止下拉刷新
     this.setData({
+      loading: false, // 正在加载
+      loadingHasData: true, //是否还有有数据
       page: 0,
       dataList: [],
     });
-    this.initArticleList();
-    // this.fetchSearchList();
+    this.fetchSearchList(true);
   },
-
+  /**
+   * 分页函数
+   */
+  fetchSearchList: function(isShowLoading) {
+    if (isShowLoading) {
+      wx.showLoading({
+        title: '正在请求数据...',
+      });
+    }
+    let that = this;
+    this.initArticleList(function(data) {
+      if (isShowLoading) {
+        wx.hideLoading()
+      }
+      //判断是否有数据，有则取数据  
+      if (data.length != 0) {
+        let dataList = [];
+        //如果isFromSearch是true从data中取出数据，否则先从原来的数据继续添加  
+        dataList = that.data.dataList.length <= 0 ? data : that.data.dataList.concat(data)
+        that.setData({
+          dataList: dataList, //获取数据数组  
+          loadingHasData: true,
+          loading: false,
+          page: (that.data.page + 1)
+        });
+      } else {
+        that.setData({
+          loading: false,
+          loadingHasData: false
+        });
+      }
+    })
+  },
   /**
    * 页面上拉触底事件的处理函数
    */
@@ -165,6 +194,6 @@ Page({
       loading: true,
       loadingHasData: true,
     });
-    // this.fetchSearchList();
+    this.fetchSearchList();
   },
 })
