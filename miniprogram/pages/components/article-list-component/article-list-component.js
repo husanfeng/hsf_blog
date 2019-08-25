efab574553f8de8faba352cd60b7414bfafbf25b
风的颜色 <35207265+husanfeng@users.noreply.github.com>
2019-08-25 11:24:22 +0800

    文章详情图片可点击查看

--- miniprogram/pages/components/article-list-component/article-list-component.js
+++ miniprogram/pages/components/article-list-component/article-list-component.js
@@ -2,23 +2,27 @@
 const app = getApp()
 Component({
   /**
-   * 组件的属性列表
+   * 组件的属性列表 
    */
   properties: {
+    navigateToParam: {
+      type: String,
+      value: ""
+    },
     articleList: {
       type: Object,
       value: []
     },
-    type:{
-      type:String,
-      value:''
+    type: {
+      type: String,
+      value: ''
     }
   },
   /**
    * 组件的初始数据
    */
   data: {
-    
+
   },
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
@@ -29,7 +33,11 @@ Component({
   // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
   attached: function() {}, // 此处attached的声明会被lifetimes字段中的声明覆盖
   ready: function() {
-
+    if (this.properties.navigateToParam != "") {
+      wx.navigateTo({
+        url: '../articleDetail/articleDetail?article_id=' + this.properties.navigateToParam,
+      })
+    }
   },
   pageLifetimes: {
     // 组件所在页面的生命周期函数
@@ -43,8 +51,8 @@ Component({
   methods: {
     click(e) {
       var item = e.currentTarget.dataset.item
-    //  wx.setStorageSync("articleDetail", item)
-    //  app.globalData.articleDetail = item;
+      //  wx.setStorageSync("articleDetail", item)
+      //  app.globalData.articleDetail = item;
       wx.navigateTo({
         url: '../articleDetail/articleDetail?article_id=' + item.article_id,
       })
--- miniprogram/pages/components/towxml-component/towxml-component.js
+++ miniprogram/pages/components/towxml-component/towxml-component.js
@@ -1,12 +1,16 @@
 // pages/components/towxml-component/towxml-component.js
 Component({
   /**
-   * 组件的属性列表
+   * 组件的属性列表 
    */
   properties: {
-    article_id:{
-      type:String,
-      value:''
+    article_id: {
+      type: String,
+      value: ''
+    },
+    isLoad: {
+      type: Boolean,
+      value: false
     }
   },
 
@@ -15,9 +19,13 @@ Component({
    */
   data: {
     article: {},
+    imgList:[],
   },
   ready() {
-    this.load();
+    if (this.properties.isLoad) {
+      this.load();
+    }
+
   },
   /**
    * 组件的方法列表
@@ -25,7 +33,6 @@ Component({
   methods: {
     load() {
       var _this = this;
-    //  var articleDetail = wx.getStorageSync("articleDetail");
       var article_id = _this.properties.article_id
       wx.showLoading({
         title: '正在加载...',
@@ -43,6 +50,14 @@ Component({
             article: res.result,
             isloading: false
           });
+          res.result.child.map((item)=>{
+            var childList  =  item.child || []
+            childList.map((child)=>{
+              if(child.tag == "image"){
+                this.data.imgList.push(child.attr.src);
+              }
+            })
+          })
         },
         fail: err => {
           console.error('[云函数]调用失败', err)
@@ -51,6 +66,23 @@ Component({
           wx.hideLoading()
         }
       })
-    }
+    },
+    /**
+     * towxml点击事件
+     * @param {} e 
+     */
+
+    __bind_tap: function(e) {
+      try {
+        if (e.target.dataset._el.attr.src != undefined) {
+          wx.previewImage({
+            current: e.target.dataset._el.attr.src,   
+            urls: this.data.imgList,
+          });
+        }
+      } catch (e) {
+        console.info(e)
+      }
+    },
   }
 })
