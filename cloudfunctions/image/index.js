439f298601f4278fa57ce6462650533be5a9d3a4
husanfeng <18755106041@163.com>
2019-12-18 17:09:46 +0800

    注释生成图像功能

--- cloudfunctions/image/index.js
+++ cloudfunctions/image/index.js
@@ -12,28 +12,37 @@ exports.main = async(event, context) => {
   /**
    * 获取云函数调用时传递的信息
    */
-  let imageUrl = event.avatar;
-  let style = event.style;
+  // let imageUrl = event.avatar;
+  // let style = event.style;
   /**
    * 下载头像并重新上传
    */
-  let filename = `generated/${Math.floor(Math.random() * 10000)}.png`;
-  let avatarBuffer = await got(imageUrl, {
-    stream: true
-  });
-  let data = await utils.upFile(avatarBuffer, filename);
+  // let filename = `generated/${Math.floor(Math.random() * 10000)}.png`;
+  // let avatarBuffer = await got(imageUrl, {
+  //   stream: true
+  // });
+  // console.log(avatarBuffer);
+  // let imgSecCheck = await cloud.openapi.security.imgSecCheck({
+  //   media: {
+  //     header: {'Content-Type': 'application/octet-stream'},
+  //     contentType: 'image/jpg',
+  //     value: avatarBuffer
+  //   }
+  // })
+  // console.log("imgSecCheck========"+imgSecCheck);
+  // let data = await utils.upFile(avatarBuffer, filename);
   /**
    * 根据文件上传情况进行相应的处理
    */
-  if (data.code === 200) {
-    return {
-      code: 200,
-      url: utils.generateURL(data.filename, `head${style}.png`)
-    }
-  } else {
-    return {
-      code: 0,
-      messge: 'code error'
-    }
-  }
+  // if (data.code === 200) {
+  //   return {
+  //     code: 200,
+  //     url: utils.generateURL(data.filename, `head${style}.png`)
+  //   }
+  // } else {
+  //   return {
+  //     code: 0,
+  //     messge: 'code error'
+  //   }
+  // }
 }
