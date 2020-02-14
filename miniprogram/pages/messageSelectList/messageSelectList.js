244020886b1dc36f20806c66b2298af585b12907
风的颜色 <35207265+husanfeng@users.noreply.github.com>
2020-02-14 21:39:07 +0800

    消息订阅

--- miniprogram/pages/messageSelectList/messageSelectList.js
+++ miniprogram/pages/messageSelectList/messageSelectList.js
@@ -21,17 +21,17 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.isSubscribe()
   },
-  
+
   isSubscribe() {
     var that = this;
     db.collection('message_subscribe').where({
       tmplIds: lessonTmplId,
       openid: wx.getStorageSync("openid"),
     }).get({
-      success: function (res) {
+      success: function(res) {
         // res.data 是包含以上定义的两条记录的数组
         console.log(res.data)
         if (res.data.length > 0) {
@@ -45,7 +45,7 @@ Page({
       tmplIds: commentReplyId,
       openid: wx.getStorageSync("openid"),
     }).get({
-      success: function (res) {
+      success: function(res) {
         // res.data 是包含以上定义的两条记录的数组
         console.log(res.data)
         if (res.data.length > 0) {
@@ -140,18 +140,19 @@ Page({
   },
   saveUser(ID, switchId) {
     var that = this;
-
-    if (!this.data.switch1Checked || !this.data.switch3Checked) {
+    var userInfo = wx.getStorageSync("userInfo")
+    if (!(switchId == 1 ? this.data.switch1Checked : this.data.switch3Checked)) {
       db.collection('message_subscribe').add({
         // data 字段表示需新增的 JSON 数据
         data: {
           //_id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
           tmplIds: ID,
+          nickName: userInfo.nickName,
           openid: wx.getStorageSync("openid"),
           date: util.formatTime(new Date()),
           done: false
         },
-        success: function (res) {
+        success: function(res) {
           // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
           console.log(res)
           if (switchId == 1) {
@@ -170,7 +171,7 @@ Page({
             duration: 2000,
           });
         },
-        fail: function (err) {
+        fail: function(err) {
           console.log(err)
           wx.showToast({
             title: '订阅失败',
@@ -252,7 +253,7 @@ Page({
         data: data,
         templateId: lessonTmplId,
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res)
       },
       fail: err => {
@@ -264,49 +265,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
