cc7a91051c5821e8604706e195328b4fa2353968
sanfeng.hu <18755106041@163.com>
2021-08-18 11:06:52 +0800

    登录api修改

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -14,10 +14,18 @@ Page({
     isShowAddPersonView: false,
     showText: '登录获取更多权限',
     openid: '',
+
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
   },
   onLoad: function () {
     if (!wx.cloud) {
       return
+    }
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
     }
     var openid = wx.getStorageSync("openid");
     if (!openid || openid == '') {
@@ -59,7 +67,7 @@ Page({
         console.log("用户的openID=" + res.result.openid)
         wx.setStorageSync("openid", res.result.openid)
         this.setData({
-          openid: openid
+          openid: res.result.openid
         })
       },
       fail: err => {
@@ -110,6 +118,20 @@ Page({
       })
       wx.setStorageSync("userInfo", e.detail.userInfo)
     }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          avatarUrl: res.userInfo.avatarUrl,
+          hasUserInfo: true
+        })
+      }
+    })
   },
   confirm(e) {
     wx.setStorageSync("userInfo", e.detail.userInfo)
