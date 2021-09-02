7a6bde2bc5b01fba966ea8e118ad2b1e70a3d3da
sanfeng.hu <18755106041@163.com>
2021-09-02 13:58:14 +0800

    登录api修改

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -63,6 +63,21 @@ Page({
     //   }
     // })
   },
+  getUserProfile() {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          avatarUrl: res.userInfo.avatarUrl,
+          hasUserInfo: true
+        })
+        wx.setStorageSync("userInfo", res.userInfo)
+      }
+    })
+  },
   getUserOpenId() {
     var _this = this;
     wx.cloud.callFunction({
